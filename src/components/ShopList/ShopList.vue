704cdd01a622d7a6b2ea5a81b1db93443ef7fa1c
王冲 <m13478440042@163.com>
2021-01-21 15:56:10 +0800

    商家页面整体搭建

--- src/components/ShopList/ShopList.vue
+++ src/components/ShopList/ShopList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="shop_container">
     <ul class="shop_list" v-if="shops.length">
-      <li class="shop_li border-1px" v-for="(shop, index) in shops" :key="index">
+      <li class="shop_li border-1px" v-for="(shop, index) in shops" :key="index" @click="$router.push('/shop')">
         <a>
           <div class="shop_left">
             <img class="shop_img" :src="imgBaseUrl + shop.image_path" alt="">
@@ -51,7 +51,7 @@ import {mapState} from 'vuex'
 import Star from '../Star/Star'
 export default {
   name: 'ShopList',
-  data() {
+  data () {
     return {
       imgBaseUrl: 'http://elm.cangdu.org/img/'
     }
--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,15 @@ import Order from '../pages/Order/Order'
 import Profile from '../pages/Profile/Profile'
 import Search from '../pages/Search/Search'
 import Login from '../pages/Login/Login'
+import Shop from '../pages/Shop/Shop'
+import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods'
+import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings'
+import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo'
 
-//声明并使用路由
+// 声明并使用路由
 Vue.use(VueRouter)
 
-//暴露路由
+// 暴露路由
 export default new VueRouter({
   routes: [
     {
@@ -45,8 +49,30 @@ export default new VueRouter({
       }
     },
     {
-      path: "/login",
+      path: '/login',
       component: Login
+    },
+    {
+      path: '/shop',
+      component: Shop,
+      children: [
+        {
+          path: '/shop/goods',
+          component: ShopGoods
+        },
+        {
+          path: '/shop/ratings',
+          component: ShopRatings
+        },
+        {
+          path: '/shop/info',
+          component: ShopInfo
+        },
+        {
+          path: '',
+          redirect: '/shop/goods'
+        }
+      ]
     }
   ]
 })
